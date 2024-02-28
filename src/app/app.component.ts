e7c0643e1bcfc3e3e5da77010cb37da2ce74f6de
Mapster <macster87@gmail.com>
2024-02-28 11:16:01 +0100

    add template syntax component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TemplateSyntaxComponent} from './template-syntax/template-syntax.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TemplateSyntaxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
