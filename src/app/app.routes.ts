d36487611aa7ac17100b278703a197af98e894b2
Mapster <macster87@gmail.com>
2024-02-29 06:55:20 +0100

    erster tag

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,12 @@ import { Routes } from '@angular/router';
 import { TemplateSyntaxComponent } from './components/template-syntax/template-syntax.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: TemplateSyntaxComponent,
-  },
-  {
-    path: 'about',
-    component: AboutPageComponent,
-  },
-  {
-    path: '**',
-    component: NotFoundPageComponent,
-  },
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: StartPageComponent },
+  { path: 'template', component: TemplateSyntaxComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: '**', component: NotFoundPageComponent },
 ];
